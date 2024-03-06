242ea691160723188c6d1fc42e7bf53f57a5538d
zupiCoder <lz79481@student.uni-lj.si>
2024-03-06 13:46:17 +0100

    uploaded files from labExs

--- vaje/vaje02/pitagorejskeTrojice/PitagorejskeTrojiceFaster.c
+++ vaje/vaje02/pitagorejskeTrojice/PitagorejskeTrojiceFaster.c
@@ -11,26 +11,30 @@ int main() {
 
   int m, n;
   int counter = 0;
-  bool foundC = false;
-
+  
   scanf("%d%d", &m, &n);
 
   for(int c = m; c <= n; c++) {
     for(int a = 1; a < c; a++) {
-      for(int b = a + 1; b < c; b++) {
-        if(isPithagorean(a,b,c) && (a != b)) {
-          counter++;  
-          foundC = true;
-          break;
-        }
-      }
-      if(foundC) {
-        foundC = false;
+      int b2 = c * c - a * a;
+      int bSqrt = round(sqrt(b2)); //zaokrozimo na celo vrednost
+      if(bSqrt * bSqrt == b2) {
+        counter++;
         break;
       }
     }
   }
 
+
+
+
+
   printf("%d \n", counter);
   return 0;
 }
+
+/*
+faster verzija prejsnjega programa,
+poskusimo se znebiti nekega gnezdenega loopa,
+da pohitrimo iskanje kombinacij dolocenih stevil,
+*/
