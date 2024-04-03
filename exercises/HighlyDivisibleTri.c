be3c12b1290f848482d0c36a6c48376de4daae3e
zupiCoder <lz79481@student.uni-lj.si>
2024-04-03 18:02:28 +0200

    vaje06

--- exercises/HighlyDivisibleTri.c
+++ exercises/HighlyDivisibleTri.c
@@ -1,31 +1,15 @@
-
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
 
 int numOfDivisors(int num) {
 
-    int divisors = 1;
-    int root = sqrt(num);
-    
-    for(int i = 1; i < root; i++) {
+    int divisors = 0;
+
+    for(int i = 1; i < sqrt(num); i++) {
         if(num % i == 0) {
             divisors++;
         }
-    }   
-    return divisors;
-}
-
-int main() {
-
-    int num = 1;
-    int counter = 1;
-    //num to have over 500 divisors
-    while(numOfDivisors(num) <= 500) {
-        counter++;
-        num += counter;
     }
 
-    printf("%d\n",num);
-    return 0;
+    return divisors;
 }
--- exercises/SummationOfPrimes.c
+++ exercises/SummationOfPrimes.c
@@ -1,33 +1,41 @@
-
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
 
-bool isPrime(int num) {
-    if (num <= 1) return false; // 0 and 1 are not prime
-    if (num == 2) return true;  // 2 is the only even prime number
-    if (num % 2 == 0) return false; // Exclude even numbers greater than 2
+bool isPrime(int num)
+{
+	if (num <= 1)
+		return false;
+	if (num == 2)
+		return true;
+	if (num % 2 == 0)
+		return false;
 
-    int root = (int)sqrt(num) + 1;
+	int root = sqrt(num);
+	for (int i = 3; i <= root; i += 2)
+	{
+		if (num % i == 0)
+		{
+			return false;
+		}
+	}
 
-    for (int i = 3; i <= root; i += 2) { // Check only odd divisors
-        if (num % i == 0) {
-            return false; // Found a factor, not a prime number
-        }
-    }
-    return true; // No factors found, is a prime number
+	return true;
 }
 
-int main () {
-    int lim = 2000000;
-    int sum = 0;
-    
-    for(int i = 2; i <= lim; i++) {
-        if(isPrime(i)) {
-            sum += i;
-        }
-    
-    }
-    printf("%d",sum);
-    return 0;
+int main()
+{
+	long long int sum = 2;
+	int lim = 2000000;
+
+	for (int i = 3; i < lim; i += 2)
+	{
+		if (isPrime(i))
+		{
+			sum += i;
+		}
+	}
+
+	printf("%lld\n", sum);
+	return 0;
 }
